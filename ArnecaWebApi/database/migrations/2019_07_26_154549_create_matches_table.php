14f4d20e777c1ba61ac8f68de5e0630e1766b202
FatmaKaya <kaya.fatma.kaya@hotmail.com>
2019-07-26 18:53:14 +0300

    created matches

---
+++ ArnecaWebApi/database/migrations/2019_07_26_154549_create_matches_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('lig');
+            $table->string('time');
+            $table->string('dateRaw');
+            $table->string('order');
+            $table->string('yer');
+            $table->string('takimA');
+            $table->string('takimB');
+            $table->string('imgA');
+            $table->string('imgB');
+            $table->string('skor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+}
